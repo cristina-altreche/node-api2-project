033999938a78fd54f4256e23b848b49f8aabc8ba
Cristina Altreche <c.altreche@gmail.com>
2020-08-04 20:39:03 -0400

    created client folder and use cors

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require('express')
+const cors = require('cors')
 const postsRouter = require('./posts/posts-router')
 const server = express()
 server.use(express.json())
 
 server.use('/api/posts', postsRouter)
+server.use(cors())
 
 // server.get('/', (req, res) => res.send('API up and running'))
 
